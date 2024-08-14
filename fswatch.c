4483974803663d138913585d393350102c58dc5d
Kévin Dunglas <kevin@dunglas.fr>
2024-08-14 14:52:47 +0200

    fix: make tests green with the race detector

--- fswatch.c
+++ fswatch.c
@@ -1,8 +1,11 @@
 #include "fswatch.h"
 #include "_cgo_export.h"
-#include <stdint.h>
 
-void process_events(fsw_cevent const *const events,
-                    const unsigned int event_num, void *data) {
+static inline void process_events(fsw_cevent const *const events,
+                                  const unsigned int event_num, void *data) {
   go_callback((fsw_cevent *)events, event_num, (uintptr_t)data);
+}
+
+void set_callback(const FSW_HANDLE handle, uintptr_t data) {
+  fsw_set_callback(handle, process_events, (void *)data);
 }
